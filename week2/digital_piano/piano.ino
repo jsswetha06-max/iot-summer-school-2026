44eff575a4ab22bff47bfbb6e7bd9b87ff3c4d8a
jsswetha06-max <jsswetha06@gmail.com>
2026-07-06 15:06:18 +0530

    feat: add major and minor scale toggle
    Added mode button functionality to switch between major and minor scales.

--- week2/digital_piano/piano.ino
+++ week2/digital_piano/piano.ino
@@ -1,18 +1,42 @@
+/*
+ * Digital Piano using Passive Buzzer
+ * Author: Swetha
+ */
+
 const int buzzer = 8;
 
 const int btnDo = 2;
 const int btnRe = 3;
 const int btnMi = 4;
 const int btnFa = 5;
+const int modeBtn = 6;
+
+bool majorScale = true;
+bool lastModeState = HIGH;
+
+int majorNotes[4] = {262, 294, 330, 349};
+int minorNotes[4] = {262, 294, 311, 349};
 
 void setup() {
+
   pinMode(btnDo, INPUT_PULLUP);
   pinMode(btnRe, INPUT_PULLUP);
   pinMode(btnMi, INPUT_PULLUP);
   pinMode(btnFa, INPUT_PULLUP);
+  pinMode(modeBtn, INPUT_PULLUP);
+
 }
 
 void loop() {
+
+  bool currentMode = digitalRead(modeBtn);
+
+  if (lastModeState == HIGH && currentMode == LOW) {
+    majorScale = !majorScale;
+    delay(200);
+  }
+
+  lastModeState = currentMode;
 
   bool b1 = digitalRead(btnDo) == LOW;
   bool b2 = digitalRead(btnRe) == LOW;
@@ -28,22 +52,22 @@ void loop() {
   }
   else if (b1) {
 
-    tone(buzzer, 262);
+    tone(buzzer, majorScale ? majorNotes[0] : minorNotes[0]);
 
   }
   else if (b2) {
 
-    tone(buzzer, 294);
+    tone(buzzer, majorScale ? majorNotes[1] : minorNotes[1]);
 
   }
   else if (b3) {
 
-    tone(buzzer, 330);
+    tone(buzzer, majorScale ? majorNotes[2] : minorNotes[2]);
 
   }
   else if (b4) {
 
-    tone(buzzer, 349);
+    tone(buzzer, majorScale ? majorNotes[3] : minorNotes[3]);
 
   }
   else {
@@ -51,6 +75,4 @@ void loop() {
     noTone(buzzer);
 
   }
-
-  delay(20);
 }
